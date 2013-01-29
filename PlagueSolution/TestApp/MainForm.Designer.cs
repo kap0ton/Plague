a278189ba4505def3d382d163c9ac42d53107503
anton kaptcov <kap_ton@me.com>
2013-01-29 18:21:00 +0400

    update test app: add create\remove funcs

--- PlagueSolution/TestApp/MainForm.Designer.cs
+++ PlagueSolution/TestApp/MainForm.Designer.cs
@@ -28,15 +28,40 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.btnCreate = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// btnCreate
+			// 
+			this.btnCreate.Location = new System.Drawing.Point(61, 41);
+			this.btnCreate.Name = "btnCreate";
+			this.btnCreate.Size = new System.Drawing.Size(75, 23);
+			this.btnCreate.TabIndex = 0;
+			this.btnCreate.Text = "Create";
+			this.btnCreate.UseVisualStyleBackColor = true;
+			this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Enabled = false;
+			this.btnDelete.Location = new System.Drawing.Point(61, 85);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(75, 23);
+			this.btnDelete.TabIndex = 1;
+			this.btnDelete.Text = "Delete";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.ClientSize = new System.Drawing.Size(344, 261);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnCreate);
 			this.Name = "MainForm";
-			this.Text = "MainForm";
+			this.Text = "Directory creation\\deletion test";
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_OnFormClosed);
 			this.Load += new System.EventHandler(this.MainForm_OnLoad);
 			this.ResumeLayout(false);
@@ -44,6 +69,9 @@
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btnCreate;
+		private System.Windows.Forms.Button btnDelete;
 	}
 }
 
--- PlagueSolution/TestApp/MainForm.cs
+++ PlagueSolution/TestApp/MainForm.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 using log4net;
 using log4net.Config;
@@ -26,6 +28,57 @@ namespace TestApp
 		private void MainForm_OnFormClosed(object sender, FormClosedEventArgs e)
 		{
 			_logger.Info("Application closed");
+		}
+
+		private string _dirPath = string.Empty;
+
+		private void btnCreate_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				_logger.Info("get assembly path");
+				var path = Assembly.GetAssembly(typeof (MainForm)).Location;
+				_logger.Info("get assembly directory");
+				var dir = Path.GetDirectoryName(path);
+				_logger.Info("combine new directory");
+				_dirPath = Path.Combine(dir, "Folder");
+				_logger.Info("create new directory");
+				Directory.CreateDirectory(_dirPath);
+				_logger.Info("directory created");
+
+				btnDelete.Enabled = true;
+				btnCreate.Enabled = false;
+			}
+			catch (Exception ex)
+			{
+				_logger.Error("directory creation failed", ex);
+			}
+			finally
+			{
+				btnCreate.Enabled = false;
+			}
+		}
+
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				_logger.Info("delete directory " + _dirPath);
+				if (string.IsNullOrEmpty(_dirPath))
+					throw new InvalidOperationException("directory path is empty");
+
+				Directory.Delete(_dirPath, true);
+				_logger.Info("directory deleted successfully");
+				btnCreate.Enabled = true;
+			}
+			catch (Exception ex)
+			{
+				_logger.Error("directory deletion failed", ex);
+			}
+			finally
+			{
+				btnDelete.Enabled = false;
+			}
 		}
 	}
 }

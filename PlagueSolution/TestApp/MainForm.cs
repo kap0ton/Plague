81406c826cf0f35c0b0ab60ad548d3652d8f15a6
anton kaptcov <kap_ton@me.com>
2013-01-29 07:54:15 +0400

    add test project

---
+++ PlagueSolution/TestApp/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using log4net;
+using log4net.Config;
+
+namespace TestApp
+{
+	public partial class MainForm : Form
+	{
+		private ILog _logger;
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_OnLoad(object sender, EventArgs e)
+		{
+			XmlConfigurator.Configure();
+			_logger = LogManager.GetLogger(typeof (MainForm));
+			//BasicConfigurator.Configure();
+
+			_logger.Info("Application started");
+		}
+
+		private void MainForm_OnFormClosed(object sender, FormClosedEventArgs e)
+		{
+			_logger.Info("Application closed");
+		}
+	}
+}
